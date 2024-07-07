aa981857ebdd23869b8ac11e2e02fcde78eec500
letifoss <letifossemale@gmail.com>
2024-07-07 18:46:11 -0300

    ajustes background

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/Footer.css'
 import '../App.css'
+import { Link } from "react-router-dom";
 
 import iconig from '../imagenes/iconig.png';
 import icontiktok from '../imagenes/icontiktok.png';
@@ -10,15 +11,15 @@ function Footer(){
         <>
         <footer className="footer d-flex justify-content-around">
             <div className="d-flex flex-column justify-content-center">
-                <a href="" className="mb-3"><b> Volver al inicio </b></a>
-                <a href="">Sobre nosotros</a>
-                <a href="">Eventos</a>
-                <a href="">Lanzamientos</a>
-                <a href="">Contacto</a>
+                <Link to="/" className="mb-3"><b> Volver al inicio </b></Link>
+                <Link to="/nosotros">Sobre nosotros</Link>
+                <Link to="/eventos">Eventos</Link>
+                <Link to="/lanzamientos">Lanzamientos</Link>
+                <Link to="/contacto">Contacto</Link>
             </div>
             <div className="d-flex align-items-center">
-                <a href="" className="p-3"><img src={iconig} alt="logoinstagram" width="30px" className="footericon" /></a>
-                <a href="" className="p-3"><img src={icontiktok} alt="logotiktok" width="30px" className="footericon" /></a>
+                <a href="https://www.instagram.com/gacetaplay/" target="_blank" className="p-3"><img src={iconig} alt="logoinstagram" width="30px" className="footericon" /></a>
+                <a href="https://www.tiktok.com/@gaceta.play" target="_blank" className="p-3"><img src={icontiktok} alt="logotiktok" width="30px" className="footericon" /></a>
             </div>
         </footer>
         </>
