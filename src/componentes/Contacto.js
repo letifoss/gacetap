622c3a7fcc35588284bbbe9d297136e94447121e
letifoss <letifossemale@gmail.com>
2024-07-07 13:36:43 -0300

    add formsubmit

--- src/componentes/Contacto.js
+++ src/componentes/Contacto.js
@@ -15,13 +15,14 @@ function Contacto(){
     <>
     <div class="d-flex flex-column align-items-center justify-content-center text">
     <h3 class="m-5 text-white text-center">Si tenés dudas o consultas, contáctanos!</h3>
-    <form action="" class="d-flex flex-column text-dark">
-        <input type="text" placeholder="Nombre" class="m-1 p-1" />
-        <input type="text" placeholder="Apellido" class="m-1 p-1" />
-        <input type="email" placeholder="E-mail" class="m-1 p-1" />
-        <textarea name="" id="" placeholder="Mensaje" rows="6" class="m-1 p-1"></textarea>
+    <form action="https://formsubmit.co/letifossemale@gmail.com" method="POST" class="d-flex flex-column text-dark" >
+        <input type="text" placeholder="Nombre completo" class="m-1 p-1" name="name"/>
+        <input type="email" placeholder="E-mail" class="m-1 p-1" name="email"/>
+        <textarea id="" placeholder="Mensaje" rows="6" class="m-1 p-1" name="subject" ></textarea>
+        <input type="hidden" name="_next" value="https://gacetap.vercel.app/contacto" />
+        <input type="hidden" name="_captcha" value="false" />
+        <button type="submit" class="p-2 mt-2 submit w-25">Enviar mensaje</button>
     </form>
-    <button type="submit" class="p-2 mt-2 submit">Enviar mensaje</button>
     </div>
     </>
     )
