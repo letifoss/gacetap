3609003c679c7306cc60d659eaac37e220a0d295
letifoss <letifossemale@gmail.com>
2024-07-14 20:34:04 -0300

    ajustes metadescription

--- src/componentes/Imagelink.js
+++ src/componentes/Imagelink.js
@@ -15,7 +15,7 @@ function Imagelink() {
     <div className="d-flex justify-content-evenly mb-5 flex-wrap align-content-center">
     <Link to="/eventos" className="imagelink">
       <div className="d-flex justify-content-center align-items-center position-relative" data-aos="fade-right">
-        <img src={foto1} alt="eventos" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0" />
+        <img src={foto1} alt="foto artistas" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0" />
         <div className="d-flex position-absolute justify-content-center align-items-center p-card-eventos image-shadow">
           <h2 className="d-flex text-center">EVENTOS</h2>
           <i className="bi bi-calendar-event ml-3 mb-2"></i>
@@ -24,7 +24,7 @@ function Imagelink() {
       </Link>
       <Link to="/lanzamientos" className="imagelink">
       <div className="d-flex justify-content-center align-items-center position-relative" data-aos="fade-up">
-        <img src={foto3} alt="lanzamientos" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0 " />
+        <img src={foto3} alt="foto productores" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0 " />
         <div className="d-flex position-absolute justify-content-center align-items-center p-card-eventos image-shadow">
           <h2 className="d-flex text-center">LANZAMIENTOS</h2>
           <i className="bi bi-skip-end-fill ml-3 mb-2"></i>
@@ -33,7 +33,7 @@ function Imagelink() {
       </Link>
       <Link to="/artistas" className="imagelink">
         <div className="d-flex justify-content-center align-items-center position-relative" data-aos="fade-left">
-          <img src={foto4} alt="conoce a nuestros artistas" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0" />
+          <img src={foto4} alt="fotos artistas" width="350px" height="367px" className="m-4 m-md-4 m-sm-4 border-0" />
           <div className="d-flex position-absolute justify-content-center align-items-center p-card-eventos image-shadow">
             <h2 className="d-flex text-center">CONOCÉ A NUESTROS ARTISTAS</h2>
           </div>
